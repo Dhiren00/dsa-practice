99afbcdf6f1ab19615c937e86f7d86933d8a0f97
Dhiren00 <redhiren@gmail.com>
2026-03-08 14:48:06 +0530

    Implement reverse string function in Solution class

--- Strings/reverse_string.cpp
+++ Strings/reverse_string.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int sat=0;
+         int n= s.size();
+         int end =n-1;
+         while(end>=sat)
+         {
+            swap(s[sat],s[end]);
+            end--;
+            sat++;
+         }
+
+        
+    }
+};
