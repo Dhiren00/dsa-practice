8264de90867bb88b5201cf0ade925091f53fe842
Dhiren00 <redhiren@gmail.com>
2026-07-17 00:05:44 +0530

    Time: 1 ms (27.48%), Space: 12.6 MB (8.2%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        queue<pair<int,int>> q;
+
+        for (int i = 0; i < tickets.size(); i++) {
+            q.push({tickets[i], i});
+        }
+
+        int time = 0;
+
+        while (!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+
+            curr.first--;
+            time++;
+
+            if (curr.first == 0) {
+                if (curr.second == k)
+                    return time;
+            } else {
+                q.push(curr);
+            }
+        }
+
+        return time;
+    }
+};
