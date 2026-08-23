9a12541faa9470c2bf27e15ea395a971ad7d2901
Dhiren00 <redhiren@gmail.com>
2026-08-23 23:28:34 +0530

    Time: 33 ms (84.74%), Space: 71.1 MB (77.06%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    void quickSort(vector<int>& nums, int low, int high) {
+
+        if (low >= high)
+            return;
+
+        int pivot = nums[low + rand() % (high - low + 1)];
+
+        int i = low;
+        int j = low;
+        int k = high;
+
+        while (j <= k) {
+
+            if (nums[j] < pivot) {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
+            else if (nums[j] > pivot) {
+                swap(nums[j], nums[k]);
+                k--;
+            }
+            else {
+                j++;
+            }
+        }
+
+        quickSort(nums, low, i - 1);
+        quickSort(nums, k + 1, high);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+
+        quickSort(nums, 0, nums.size() - 1);
+
+        return nums;
+    }
+};
